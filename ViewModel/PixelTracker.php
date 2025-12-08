5bcb6a79f525c88408a5381c09284b464a4e1dbd
RaduRaileanu <rraileanu@gmail.com>
2025-12-08 18:02:38 +0200

    Add the iframe

---
+++ ViewModel/PixelTracker.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace TwoPerformant\BusinessLeagueMarketing\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use TwoPerformant\BusinessLeagueMarketing\Model\TransactionInfo;
+use TwoPerformant\BusinessLeagueMarketing\Model\Config;
+
+/**
+ * PixelTracker view model for the BusinessLeagueMarketing module
+ *
+ * @since 1.0.0
+ */
+class PixelTracker implements ArgumentInterface
+{
+
+    /**
+     * @var array|null
+     */
+    protected $transactionInfo;
+
+    /**
+     * @var Config
+     */
+    protected $config;
+
+    /**
+     * Constructor
+     *
+     * @param TransactionInfo $transactionInfo
+     * @param Config $config
+     */
+    public function __construct(TransactionInfo $transactionInfo, Config $config)
+    {
+        $this->transactionInfo = $transactionInfo->getTransactionInfo();
+        $this->config = $config;
+    }
+
+    /**
+     * Generates the full iframe URL to track the current order
+     *
+     * @return string
+     */
+    public function getIframeUrl(): string
+    {
+        if(empty($this->transactionInfo)){
+            return '';
+        }
+
+        // get the iframe base URL and the pixel identifiers from the config
+        $iframeBaseUrl = $this->config->getIframeUrl();
+        $campaignUnique = $this->config->getCampaignUnique();
+        $confirm = $this->config->getConfirm();
+
+        // initialize the total value and the array that will hold the name of the items in the order
+        $totalValue = 0;
+        $descriptionParts = [];
+
+        // loop through the order items and calculate the total value and the description
+        foreach($this->transactionInfo['items'] as $item){
+            $totalValue += (float)$item['value'] * (int)$item['quantity'];
+            $descriptionParts[] = (string)$item['name'];
+        }
+
+        // generate the description string and format the total value as a float with 2 decimal places
+        $description = implode(', ', $descriptionParts);
+        $totalValue = number_format((float)$totalValue, 2, '.', '');
+
+        // construct the query arguments array
+        $queryArgs = [
+            'campaign_unique' => $campaignUnique,
+            'confirm' => $confirm,
+            'value' => $totalValue,
+            'description' => $description,
+        ];
+
+        // generate the query string
+        $queryString = http_build_query($queryArgs);
+
+        // return the full iframe URL
+        return $iframeBaseUrl . '?' . $queryString;
+    }
+}
