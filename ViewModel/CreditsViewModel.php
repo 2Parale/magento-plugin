c3552d254fd480ddfd79169e87efbd460b72ea0f
RaduRaileanu <rraileanu@gmail.com>
2026-03-10 13:29:55 +0200

    Wire enable setting to credits text insertion

--- ViewModel/CreditsViewModel.php
+++ ViewModel/CreditsViewModel.php
@@ -68,4 +68,17 @@ class CreditsViewModel implements ArgumentInterface
             ScopeInterface::SCOPE_STORE
         );
     }
+
+    /**
+     * Returns whether the credits are enabled from config.
+     *
+     * @return bool
+     */
+    public function isEnabled(): bool
+    {
+        return (bool) $this->scopeConfig->getValue(
+            self::CONFIG_PATH_PREFIX . 'enabled',
+            ScopeInterface::SCOPE_STORE
+        );
+    }
 }
